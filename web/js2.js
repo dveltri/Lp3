1bf01930cfb65834c1124dcdf9106b09074de79a
dveltri <dveltri@gmail.com>
2021-06-12 15:25:42 -0300

    change get error file at seee

--- web/js2.js
+++ web/js2.js
@@ -497,7 +497,9 @@ function ShowErrorFileList()
 		out+="<a href=\""+PrgEd[SrcIdx].host+(Errors[x].Path+"/").replace("//","/")+HTMLEncode(Errors[x].Name)+"?WAC="+WAC+"\" target=\"_blank\">\n";
 		out+="<img src=\"../../img/save1.png\" width=\"20\" height=\"20\" border=\"0\" />";
 		out+="</a>";
-		out+="<a href=\"\" onclick=\"UpFile='"+HTMLEncode(Errors[x].Name)+"';UpPath='"+Errors[x].Path+"';UpType='txt';rcvERR(Errors["+x+"].Datos);return false\">\n";
+		out+="<a href=\"\" onclick=\"UpFile='"+HTMLEncode(Errors[x].Name)+"';UpPath='"+Errors[x].Path+"';UpType='txt';\
+		GetUrl(PrgEd[SrcIdx].host+"/"+Errors["+x+"].Path+"/"+Errors["+x+"].Name, rcv_err_query);return false\">\n";
+		//rcvERR(Errors["+x+"].Datos);return false\">\n";
 		out+="[";
 		if(Errors[x].Path=="/err" || Errors[x].Path=="/err/")
 			out+=Str_General;
@@ -553,6 +555,17 @@ function UpdateErrorList(Datos)
 		Errors.splice(DelErr,1);
 	}
 	ReDraw(moni_errors);
+}
+
+function rcv_err_query(Datos)
+{
+	if(Datos.status==200)
+	{
+		Datos=Datos.responseText;
+		Datos=Datos.trim();
+		Datos=RemComment(Datos)
+		rcvERR(Datos);
+	}
 }
 
 function rcvERR(Dados)
